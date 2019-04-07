c6a7fd9598391949d3349eca3e3302a0b93f0421
Bruno <brunopjsw@gmail.com>
2019-04-07 12:53:58 -0300

    fudeu

--- js/dashboard.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    document.querySelectorAll('tbody tr').forEach(function(element) {
-        element.addEventListener('click', function() {
-            window.location.href = '/detalhe-navio-v2.html';
-        })
-    })
-})
